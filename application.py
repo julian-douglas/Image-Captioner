70804b2b49e2d7d8518d964b2e46a9e1d4963fe4
Julian Douglas <84514009+julian-douglas@users.noreply.github.com>
2024-01-24 11:46:47 +0000

    Add files via upload

---
+++ application.py
@@ -0,0 +1,57 @@
+from PIL import Image
+from transformers import BlipProcessor, BlipForConditionalGeneration
+from fastapi import FastAPI, File, UploadFile
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from pathlib import Path
+from fastapi.templating import Jinja2Templates
+from jinja2 import Environment, FileSystemLoader
+
+
+# Set up Jinja2 environment
+template_env = Environment(loader=FileSystemLoader(""))
+upload_form = template_env.get_template("index.html")
+
+
+application = app = FastAPI()
+application.mount("/uploaded_images", StaticFiles(directory="uploaded_images"), name="uploaded_images")
+templates = Jinja2Templates(directory="")
+
+# CORS configuration
+origins = ["*"] 
+application.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
+
+@application.get("/", response_class=HTMLResponse)
+async def get_root():
+    return templates.TemplateResponse("index.html", {"request": "root"})
+
+@application.post("/predict", response_class=HTMLResponse)
+async def predict(file: UploadFile = File(...)):
+    contents = await file.read()
+
+    # Save the uploaded image to the 'uploaded_images' directory
+    save_path = Path("uploaded_images") / file.filename
+    with open(save_path, "wb") as image_file:
+        image_file.write(contents)
+
+    raw_image = Image.open(save_path).convert('RGB')
+
+    text = ""
+    inputs = processor(raw_image, text, return_tensors="pt")
+
+    out = model.generate(**inputs)
+    caption = processor.decode(out[0], skip_special_tokens=True)
+
+    display_style = "" if caption else "none"
+
+    return JSONResponse(content={"caption": caption, "display_style": display_style})
---
+++ functions.js
@@ -0,0 +1,201 @@
+const form = document.getElementById('upload-form');
+const fileInput = document.getElementById('file');
+const changeFileBtn = document.getElementById('changeFileBtn');
+const submitButton = document.getElementById('submit-button');
+
+changeFileBtn.addEventListener('click', function(event) {
+    event.preventDefault(); 
+    fileInput.click();
+});
+
+fileInput.addEventListener('change', function() {
+    displayFileName();
+});
+
+
+function handleFormSubmission() {
+    const loadingAnimationElement = document.getElementById('loading-animation');
+    const captionElement = document.getElementById('caption');
+
+    loadingAnimationElement.style.display = 'block';
+
+    fetch(form.action, {
+        method: form.method,
+        body: new FormData(form),
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            captionElement.textContent = data.caption;
+            captionElement.style.display = data.display_style;
+            loadingAnimationElement.style.display = 'none';
+            const captionText = document.getElementById('caption');
+            captionText.style.display = "";
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            loadingAnimationElement.style.display = 'none';
+        });
+}
+
+// Add an event listener for the form submission
+form.addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+    handleFormSubmission();
+});
+
+// Add an event listener for the submit button
+submitButton.addEventListener('click', function () {
+    handleFormSubmission();
+});
+
+var label = "Drag & Drop or Click to Choose File";
+var fileUploaded = false;
+
+document.getElementById("drag-and-drop-text").innerHTML = label;
+const changeText = document.getElementById('changeFileBtn');
+changeText.style.display = "none";
+
+function displayFileName() {
+    const fileInput = document.getElementById('file');
+    const fileLabel = document.getElementById('file-label');
+    const dragText = document.getElementById('drag-and-drop-text');
+    
+    const submitButton = document.getElementById('submit-button');
+
+    const fileName = fileInput.files[0].name;
+    fileUploaded = true;
+    fileLabel.querySelector('span').textContent = fileName;
+
+    // Display the image preview and button
+    const previewImage = document.getElementById('preview');
+    previewImage.style.display = "none";
+
+    // Show the submit button
+    submitButton.style.display = "inline-block";
+    changeText.style.display = "";
+ 
+
+    // Get the file input value and set it as the background image for file-label
+    const imageUrl = URL.createObjectURL(fileInput.files[0]);
+    fileLabel.style.backgroundImage = `url(${imageUrl})`;
+    fileLabel.style.backgroundSize = 'cover';
+
+    // Get the dimensions of the image and set the width and height of the file-label element
+    const img = new Image();
+    img.src = imageUrl;
+    img.onload = function () {
+        fileLabel.style.width = img.width + 'px';
+        fileLabel.style.height = img.height + 'px';
+    };
+
+    document.getElementById("drag-and-drop-text").innerHTML = label;
+
+    previewImage.style.display = "none";
+    const previewButton = document.getElementById('submit-button');
+    previewButton.style.display = "";
+    dragText.style.display = "none";
+  
+    const preview = document.getElementById('preview');
+    console.log(changeText);
+
+    
+
+}
+
+
+form.addEventListener('dragover', function (e) {
+    e.preventDefault();
+    form.classList.add('dragover');
+});
+
+form.addEventListener('dragleave', function () {
+    form.classList.remove('dragover');
+});
+
+form.addEventListener('drop', function (e) {
+    e.preventDefault();
+    form.classList.remove('dragover');
+
+    const fileInput = document.getElementById('file');
+    fileInput.files = e.dataTransfer.files;
+    displayFileName();
+});
+
+const imageFiles = ['1000268201_693b08cb0e.jpg',
+    '1001773457_577c3a7d70.jpg',
+    '1002674143_1b742ab4b8.jpg',
+    '1003163366_44323f5815.jpg',
+    '1007129816_e794419615.jpg',
+    '1007320043_627395c3d8.jpg',
+    '1009434119_febe49276a.jpg',
+    '1012212859_01547e3f17.jpg',
+    '1015118661_980735411b.jpg',
+    '1015584366_dfcec3c85a.jpg',
+    '101654506_8eb26cfb60.jpg',
+    '101669240_b2d3e7f17b.jpg',
+    '1016887272_03199f49c4.jpg',
+    '1019077836_6fc9b15408.jpg',
+    '1019604187_d087bf9a5f.jpg',
+    '1020651753_06077ec457.jpg',
+    '1022454332_6af2c1449a.jpg',
+    '1022454428_b6b660a67b.jpg',
+    '1022975728_75515238d8.jpg'];
+
+
+
+const imageCaptions = [
+    'A child in a pink dress is climbing up a set of stairs in an entry way.',
+    'A black dog and a spotted dog are fighting.',
+    'A little girl covered in paint sits in front of a painted rainbow with her hands in a bowl.',
+    'A man lays on a bench while his dog sits by him.',
+    'A man in an orange hat starring at something.',
+    'A child playing on a rope net.',
+    'A black and white dog is running in a grassy garden surrounded by a white fence.',
+    'A dog shakes its head near the shore  with a red ball next to it.',
+    'A boy smiles in front of a stony wall in a city.',
+    'A black dog leaps over a log.',
+    'A brown and white dog is running through the snow.',
+    'A man in a hat is displaying pictures next to a skier in a blue hat.',
+    'A collage of one person climbing a cliff.',
+    'A brown dog chases the water from a sprinkler on a lawn.',
+    'A dog prepares to catch a thrown object in a field with nearby cars.',
+    'A black and white dog jumping in the air to get a toy.',
+    'A child and a woman are at waters edge in a big city.',
+    'A couple and an infant being held by the male sitting next to a pond with a near by stroller.',
+    'A black dog running in the surf.',
+    'A man drilling a hole in the ice.'
+]
+
+// Function to preload images
+function preloadImages() {
+    imageFiles.forEach((imageFile) => {
+        const img = new Image();
+        img.src = `examples/${imageFile}`; 
+    });
+}
+
+// Function to display images and captions in a slideshow
+function startSlideshow() {
+    const slideshow = document.getElementById('slideshow');
+    let currentIndex = 0;
+
+    function showNextImage() {
+        slideshow.innerHTML = `<img src="examples/${imageFiles[currentIndex]}" alt="${imageCaptions[currentIndex]}">
+                                   <div class="caption">${imageCaptions[currentIndex]}</div>`;
+        currentIndex = (currentIndex + 1) % imageFiles.length;
+    }
+
+    showNextImage();
+
+    setInterval(showNextImage, 5000);
+}
+
+window.onload = function () {
+    preloadImages();
+    startSlideshow();
+};
